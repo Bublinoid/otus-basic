286a2ef80819087b28100f6f80cbb9e45e178bdc
Hleb <gleb.qb@gmail.com>
2024-08-14 21:14:35 +0200

    lesson-19

---
+++ lesson-19/src/main/java/ru/bublinoid/otusbasic/entity/Apple.java
@@ -0,0 +1,9 @@
+package ru.bublinoid.otusbasic.entity;
+
+public class Apple extends Fruit {
+
+    public Apple(double weight) {
+        super(weight);
+    }
+
+}
---
+++ lesson-19/src/main/java/ru/bublinoid/otusbasic/entity/Orange.java
@@ -0,0 +1,8 @@
+package ru.bublinoid.otusbasic.entity;
+
+public class Orange extends Fruit {
+
+    public Orange(double weight) {
+        super(weight);
+    }
+}
