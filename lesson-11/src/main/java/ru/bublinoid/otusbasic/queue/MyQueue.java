4b0e9b3b0665eb72e285df98da2e1c752b40a98d
Hleb <gleb.qb@gmail.com>
2024-08-14 20:21:51 +0200

    lesson-11

---
+++ lesson-11/src/main/java/ru/bublinoid/otusbasic/queue/MyQueue.java
@@ -0,0 +1,20 @@
+package ru.bublinoid.otusbasic.queue;
+
+import ru.bublinoid.otusbasic.inkedlist.MyLinkedList;
+
+public class MyQueue<T> {
+
+    private MyLinkedList<T> linkedList;
+
+    public MyQueue() {
+        this.linkedList = new MyLinkedList<>();
+    }
+
+    public void offer(T data) {
+        linkedList.addLast(data);
+    }
+
+    public T poll() {
+        return linkedList.getSize() == 0 ? null : linkedList.remove(0);
+    }
+}
